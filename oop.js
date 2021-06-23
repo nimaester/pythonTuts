9d3d11b2900613be4fef57a28f07a66255161508
nimaester <nimaester@gmail.com>
2021-06-23 03:12:49 -0700

    hangman game using oop

--- oop.js
+++ oop.js
@@ -1,12 +1,55 @@
-class Hangman {
-  constructor(word, guess) {
-    this.word = word;
-    this.guess = guess;
+class Rules {
+  constructor() {
+    this.rules = "THIS IS THE RULES";
+  }
+
+  trial() {
+    return this.rules;
   }
 }
 
-const h1 = new Hangman("pete", 4);
-const h2 = new Hangman("hacker", 3);
+class Hangman extends Rules {
+  constructor(word, guesses) {
+    super();
+    this.word = word.toLowerCase().split("");
+    this.guesses = guesses;
+    this.lettersUsed = [];
+  }
 
-console.log(h1);
-console.log(h2);
+  getPuzzle() {
+    let guess = "";
+    this.word.forEach((letter) => {
+      if (this.lettersUsed.includes(letter) || letter === " ") {
+        guess += letter;
+      } else {
+        guess += "*";
+      }
+    });
+
+    if (guess === this.word.join("")) {
+      return `You solved it! The word is "${guess}"`;
+    } else {
+      return guess;
+    }
+  }
+
+  makeGuess(char) {
+    let letter = char.toLowerCase();
+    if (this.lettersUsed.includes(letter)) {
+      return "Use another letter";
+    } else {
+      this.lettersUsed.push(letter);
+      this.guesses -= 1;
+      if (this.guesses < 0) {
+        return "You ran out of guesses";
+      }
+      return this.getPuzzle();
+    }
+  }
+}
+
+const h1 = new Hangman("kitten", 10);
+
+window.addEventListener("keypress", (event) => {
+  console.log(h1.makeGuess(event.key));
+});
